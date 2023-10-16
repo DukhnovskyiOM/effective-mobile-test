c47a0263ccb8e2c1d5c44fdfc1cd81bcb7ccca8c
AleksandrDukhn1 <sashkadom@gmail.com>
2023-10-16 21:15:51 +0300

    db

--- server/db.js
+++ server/db.js
@@ -2,10 +2,10 @@ const Pool = require("pg").Pool
 
 const pool = new Pool({
     user: "postgres",
-    password: "Dom0935924729#",
+    password: "postgres",
     host: "localhost",
     port: 5432,
-    database: "pernusers"
-})
+    database: "pernusers",
+});
 
 module.exports = pool
